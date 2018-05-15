9e870eb8d6322c629b6548aef87c6c972daa16e0
Big <wattanachai.pra@ascendcorp.com>
2018-05-15 22:12:49 +0700

    Initial a todo list line bot project

---
+++ src/main/java/com/bot/wattanachaitodolist/WattanachaiTodolistApplication.java
@@ -0,0 +1,12 @@
+package com.bot.wattanachaitodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WattanachaiTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WattanachaiTodolistApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bot/wattanachaitodolist/WattanachaiTodolistApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bot.wattanachaitodolist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WattanachaiTodolistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
