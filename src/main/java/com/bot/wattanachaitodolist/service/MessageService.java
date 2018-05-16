cab5c7692d49d00c7ea532bc759869bbd169a919
Big <wattanachai.pra@ascendcorp.com>
2018-05-16 16:51:19 +0700

    third test line bot (response messageEvent)

--- src/main/java/com/bot/wattanachaitodolist/service/MessageService.java
+++ src/main/java/com/bot/wattanachaitodolist/service/MessageService.java
@@ -3,6 +3,7 @@ package com.bot.wattanachaitodolist.service;
 import com.bot.wattanachaitodolist.constant.MessageCommand;
 import com.bot.wattanachaitodolist.domain.Todo;
 import com.bot.wattanachaitodolist.repository.TodoRepository;
+import com.bot.wattanachaitodolist.util.JsonMapper;
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.MessageEvent;
@@ -15,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.constraints.NotNull;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -34,22 +34,30 @@ public class MessageService {
     }
 
     public void messageCommand(MessageEvent<TextMessageContent> messageEvent) {
+
         TextMessageContent message = messageEvent.getMessage();
         log.info("Got text message from {}: {}", messageEvent.getReplyToken(), message.getText());
+        log.info("Source Info : {}", messageEvent.getSource());
 
         MessageCommand command = parseMessageCommand(message.getText());
 
-        switch (command) {
-            case CREATE_TODO:
-                log.info("CREATE_TODO from {}: {}", messageEvent.getReplyToken(), message.getText());
-                executeCreateTodo(messageEvent.getReplyToken(), messageEvent, message);
-            case EDIT_TODO:
-                log.info("EDIT_TODO from {}: {}", messageEvent.getReplyToken(), message.getText());
-                executeEditTodoList();
-            default:
-                log.info("default {}", message.getText());
-
+        if ("edit".equalsIgnoreCase(message.getText())) {
+            this.replyText(messageEvent.getReplyToken(), "https://medium.com/@Big.Wattanachai");
+        } else {
+            this.replyText(messageEvent.getReplyToken(), JsonMapper.toJson(messageEvent).orElse("cannot map json"));
         }
+
+//        switch (command) {
+//            case CREATE_TODO:
+//                log.info("CREATE_TODO from {}: {}", messageEvent.getReplyToken(), message.getText());
+//                executeCreateTodo(messageEvent.getReplyToken(), messageEvent, message);
+//            case EDIT_TODO:
+//                log.info("EDIT_TODO from {}: {}", messageEvent.getReplyToken(), message.getText());
+//                executeEditTodoList();
+//            default:
+//                log.info("default {}", message.getText());
+//
+//        }
     }
 
     private void executeCreateTodo(String replyToken, MessageEvent<TextMessageContent> messageEvent,
@@ -59,7 +67,7 @@ public class MessageService {
         todo.setCompleted(false);
         todo.setTask(message.getText());
         todoRepository.save(todo);
-        String response = message.getText() + "Your todo has been created successfully";
+        String response = "Your todo has been created successfully : " + message.getText();
         this.replyText(replyToken, response);
     }
 
@@ -67,11 +75,11 @@ public class MessageService {
 
     }
 
-    protected void reply(@NotNull String replyToken, @NotNull Message message) {
+    protected void reply(String replyToken, Message message) {
         reply(replyToken, Collections.singletonList(message));
     }
 
-    protected void reply(@NotNull String replyToken, @NotNull List<Message> messages) {
+    protected void reply(String replyToken, List<Message> messages) {
         try {
             BotApiResponse apiResponse = lineMessagingClient.replyMessage(new ReplyMessage(replyToken, messages)).get();
         } catch (InterruptedException | ExecutionException e) {
@@ -79,7 +87,7 @@ public class MessageService {
         }
     }
 
-    protected void replyText(@NotNull String replyToken, @NotNull String message) {
+    protected void replyText(String replyToken, String message) {
         if (replyToken.isEmpty()) {
             throw new IllegalArgumentException("replyToken must not be empty");
         }
