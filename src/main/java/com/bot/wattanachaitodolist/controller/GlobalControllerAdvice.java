b80fb1eb5a643859f8ba32802aa3f87fddb40335
Big <wattanachai.pra@ascendcorp.com>
2018-05-19 17:16:58 +0700

    Added handle NotFoundException in GlobalControllerAdvice

--- src/main/java/com/bot/wattanachaitodolist/controller/GlobalControllerAdvice.java
+++ src/main/java/com/bot/wattanachaitodolist/controller/GlobalControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.bot.wattanachaitodolist.controller;
 
 import com.bot.wattanachaitodolist.exception.NotAuthorizedException;
+import com.bot.wattanachaitodolist.exception.NotFoundException;
 import com.bot.wattanachaitodolist.model.ApiResponse;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -12,5 +13,10 @@ public class GlobalControllerAdvice {
     @ExceptionHandler(value = NotAuthorizedException.class)
     public HttpEntity<ApiResponse> handleNotAuthorizedException() {
         return new ApiResponse("Unauthorized", null).build(HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(value = NotFoundException.class)
+    public HttpEntity<ApiResponse> handleNotFoundException(NotFoundException exception) {
+        return new ApiResponse(exception.getMessage(), null).build(HttpStatus.NOT_FOUND);
     }
 }
